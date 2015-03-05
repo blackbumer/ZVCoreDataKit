a5c83d082f3a02efa1fbfeb516b141fb1978ea78
Danil Zvyagintsev <danil.zvyagintsev@gmail.com>
2015-03-05 00:57:39 -0500

    Initial Commit

---
+++ ZVCoreDataKit/ZVCoreDataKit.h
@@ -0,0 +1,19 @@
+//
+//  ZVCoreDataKit.h
+//  ZVCoreDataKit
+//
+//  Created by Danil Zvyagintsev on 3/5/15.
+//  Copyright (c) 2015 Danil Zvyagintsev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZVCoreDataKit.
+FOUNDATION_EXPORT double ZVCoreDataKitVersionNumber;
+
+//! Project version string for ZVCoreDataKit.
+FOUNDATION_EXPORT const unsigned char ZVCoreDataKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZVCoreDataKit/PublicHeader.h>
+
+
